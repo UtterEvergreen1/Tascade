170e3c8e48672de54612cf4d5e64236d8fc87ec0
Daniel <102389559+UtterEvergreen1@users.noreply.github.com>
2025-12-03 01:08:57 -0800

    Set default values in local storage and fixed shop.html tags.

--- js/unlockables.js
+++ js/unlockables.js
@@ -1,6 +1,11 @@
 let unlocked = JSON.parse(localStorage.getItem("unlockedItems") || "[]");
-let unlockedThemes = JSON.parse(localStorage.getItem("unlockedThemes") || '["light"]');
+let unlockedThemes = JSON.parse(localStorage.getItem("unlockedThemes") || "[]");
 let unlockedFonts = JSON.parse(localStorage.getItem("unlockedFonts") || "[]");
+
+if (!unlockedThemes.includes("light")) {
+    unlockedThemes.push("light");
+    localStorage.setItem("unlockedThemes", JSON.stringify(unlockedThemes));
+}
 
 if (!unlockedFonts.includes("segoeUI")) {
     unlockedFonts.push("segoeUI");
